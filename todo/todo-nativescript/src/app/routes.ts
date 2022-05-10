1ab4095f521efa78ac01e60e0bd0313890f2cdfa
Wenqi He <hewenqi96@gmail.com>
2022-05-10 14:07:03 +0800

    rm

--- todo/todo-nativescript/src/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from '@src/app/home/home.component';
-import { DetailsComponent } from '@src/app/details/details.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'details/:id', component: DetailsComponent }
-];
